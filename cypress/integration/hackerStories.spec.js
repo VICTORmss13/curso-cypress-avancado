bf1a0b7280a18445c23635998fbd9627934a8267
Victor Mendes <victormendes13@gmail.com>
2023-12-28 19:19:06 -0300

    Aula 2

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -1,9 +1,15 @@
 describe('Hacker Stories', () => {
   beforeEach(() => {
-    cy.visit('/')
 
-    cy.assertLoadingIsShownAndHidden()
-    cy.contains('More').should('be.visible')
+    cy.intercept(
+      'GET',
+      '**/search?query=React&page=0'
+    ).as('getStories');
+    cy.visit('/')
+    cy.wait('@getStories')
+
+    // cy.assertLoadingIsShownAndHidden()
+    // cy.contains('More').should('be.visible')
   })
 
   it('shows the footer', () => {
