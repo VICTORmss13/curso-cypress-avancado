0a2cb23c7e8d7904d532eecfc5775ba09e55920d
Victor Mendes <victormendes13@gmail.com>
2023-12-29 13:27:52 -0300

    User don't wait request

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -1,15 +1,17 @@
 describe('Hacker Stories', () => {
   beforeEach(() => {
 
-    cy.intercept(
-      'GET',
-      '**/search?query=React&page=0'
-    ).as('getStories');
+    cy.intercept({
+      method: 'GET',
+      pathname: '**/search',
+      query: {
+        query: 'React',
+        page: '0'
+      }
+    }).as('getStories')
+
     cy.visit('/')
     cy.wait('@getStories')
-
-    // cy.assertLoadingIsShownAndHidden()
-    // cy.contains('More').should('be.visible')
   })
 
   it('shows the footer', () => {
