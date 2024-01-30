4e035616f3723bc73ab6f97514d77e0635f079c8
Victor Mendes <victormendes13@gmail.com>
2024-01-29 22:02:11 -0300

    Aula 8

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -336,3 +336,23 @@ context('Errors', () => {
       .should('be.visible')
   })
 })
+
+
+it.only('shows a "Loading ..." state before showing the results', () => {
+  cy.intercept(
+    'GET',
+    '**/search**',
+    {
+      delay: 1000,
+      fixture: 'stories'
+    }
+  ).as('getDelayedStories')
+
+  
+  cy.visit('/')
+
+  cy.assertLoadingIsShownAndHidden()
+  cy.wait('@getDelayedStories')
+
+  cy.get('.item').should('have.length', 2)
+})
